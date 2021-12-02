e370d7f7e25e5d5689a72a3d318687e4d71951af
Parag Pathak <31978917+paragpathak2006@users.noreply.github.com>
2021-12-02 01:56:49 -0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
+#    @users = User.where(activated: true).paginate(page: params[:page])
     @users = User.paginate(page: params[:page])
   end
 
